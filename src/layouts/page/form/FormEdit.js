2c92ee098564f1499fee2fa5259ced13e747111a
chenwentao@congzhitech.com <chenwentao@congzhitech.com>
2020-03-11 22:07:32 +0800

    form edit

--- src/layouts/page/form/FormEdit.js
+++ src/layouts/page/form/FormEdit.js
@@ -2,15 +2,62 @@ import React from 'react';
 import Preview from "./component/Preview";
 import {maps} from './data'
 import Setting from "./component/Setting";
+import {Button, Card, message} from "antd";
+import {request} from "../utils/request";
 
 const FormEdit = props => {
   const [properties, setProperties] = React.useState([]);
   const [active, setActive] = React.useState(-1)
-  const allColumn = (key) => {
-    setProperties(properties => [...properties, {key}])
+  const {match, history} = props;
+  const id = parseInt(match.params.id || '0');
+  React.useEffect(() => {
+    if (id > 0) {
+      request('/form/item', {Id: id}).then(item => {
+        const {Id, Content} = item;
+        const properties = Content ? JSON.parse(Content) : []
+        setProperties(properties)
+      })
+    }
+  }, [id])
+  const allColumn = (key) => setProperties(properties => [...properties, {key}])
+  const gridStyle = {
+    flex: 3,
+  };
+  const gridCenterStyle = {
+    flex: 4,
+    border: 'none',
+    overflow: 'auto'
+  };
+  // const FormItem = e => {
+  //   e.stopPropagation();
+  //   request("/form/new", {content: JSON.stringify(properties)}).then(res => {
+  //     if (res) {
+  //       message.success("success");
+  //     }
+  //   })
+  // }
+  const handleSubmit = e => {
+    e.stopPropagation();
+    let data = {};
+    if (id > 0) {
+      data.id = id
+    }
+    data.content = JSON.stringify(properties)
+    request("/form/new", data).then(res => {
+      if (res) {
+        history.goBack();
+        message.success("success");
+      }
+    })
   }
-  return <div style={{flex: 1, padding: 20, display: 'flex', background: '#ffffff'}}>
-    <div style={{flexBasis: 300}}>
+  return <Card
+    size="small"
+    extra={<Button onClick={handleSubmit}>Save</Button>}
+    bordered={null}
+    style={{flex: 1, overflow: 'auto', display: 'flex', flexFlow: 'column'}}
+    bodyStyle={{flex: 1, display: 'flex', overflow: 'auto'}}
+  >
+    <Card.Grid style={gridStyle}>
       {Object.values(maps).map(item => {
         return <div key={item.key} onClick={() => {
           allColumn(item.key)
@@ -19,11 +66,11 @@ const FormEdit = props => {
           {item.label}
         </div>
       })}
-    </div>
-    <div style={{flex: 1, margin: "0 10px", overflow: 'auto'}}>
+    </Card.Grid>
+    <Card.Grid style={gridCenterStyle}>
       <Preview properties={properties} active={active} changeActive={(newActive) => setActive(newActive)}/>
-    </div>
-    <div style={{flexBasis: 300}}>
+    </Card.Grid>
+    <Card.Grid style={gridStyle}>
       {active >= 0 && <Setting
         property={properties[active]}
         changeItem={(item) => {
@@ -31,7 +78,7 @@ const FormEdit = props => {
           newProperties[active] = item;
           setProperties(newProperties)
         }}/>}
-    </div>
-  </div>
+    </Card.Grid>
+  </Card>
 }
 export default FormEdit
--- src/layouts/page/form/FormPage.js
+++ src/layouts/page/form/FormPage.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import {Button, Card, Table} from "antd";
 import history from "../../history";
+import {request} from "../utils/request";
 
 const FormPage = props => {
-  const newForm = () => {
-    history.push("/form-edit/0")
+  const [list, setList] = React.useState([]);
+
+  const newForm = (id = '0') => {
+    history.push(`/form-edit/${id}`)
   }
+  React.useEffect(() => {
+    request("/form/list", {}).then(list => setList(list))
+  }, [])
+  const columns = [
+    {
+      title: 'name',
+      key: 'name',
+      dataIndex: 'Id'
+    },
+    {
+      title: 'createdAt',
+      key: 'createdAt',
+      dataIndex: 'createdAt'
+    },
+    {
+      title: 'createdBy',
+      key: 'createdBy',
+      dataIndex: 'createdBy'
+    },
+    {
+      title: 'option',
+      key: 'option',
+      dataIndex: 'option',
+      render: (text, item) => (<><a onClick={() => newForm(item.Id)}>edit </a> <a>delete</a></>)
+    }
+  ]
   return <div style={{flex: 1}}>
     <Card bordered={null}>
       <div style={{marginBottom: 10}}>
-        <Button onClick={newForm}>创建表单</Button>
+        <Button onClick={() => newForm()}>创建表单</Button>
       </div>
-      <Table/>
+      {list && <Table
+        dataSource={list}
+        columns={columns}
+        rowKey={record => record.Id.toString()}
+      />}
     </Card>
 
   </div>
